5bd46765c72914fcb634f066453b6d2b980f911d
Gerald <geraldjoly191@gmail.com>
2025-01-04 21:16:39 -0300

    All the imputs ar required to send a car to the server now

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -13,19 +13,19 @@ const CarForm = ({addCar}) => {
     <form onSubmit={handleSubmit(submit)}>
         <h2>Card Form</h2>
         <label htmlFor="brand">Brand</label>
-        <input type="text" id='brand' {...register('brand')}/>
+        <input type="text" id='brand' {...register('brand')} required/>
         
         <label htmlFor="model">model</label>
-        <input type="text" id='model'{...register('model')}/>
+        <input type="text" id='model'{...register('model')} required/>
         
         <label htmlFor="color">color</label>
-        <input type="text" id='color'{...register('color')}/>
+        <input type="text" id='color'{...register('color')} required/>
         
         <label htmlFor="price">price</label>
-        <input type="text" id='price' {...register('price')}/>
+        <input type="text" id='price' {...register('price')} required/>
         
         <label htmlFor="image">image</label>
-        <input type="text" id='image'{...register('image')}/>
+        <input type="text" id='image'{...register('image')} required/>
         <button>submit</button>
     </form>
   )
