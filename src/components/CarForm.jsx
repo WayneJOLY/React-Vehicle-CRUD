563a4a2232c33875072b16f9185bbbbe192c8146
Gerald <geraldjoly191@gmail.com>
2025-01-09 09:04:21 -0300

    I modified the content and the compounds of the Form

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import '../App'
 
 const CarForm = ({addCar,modifyCar,updateCar}) => {
 
@@ -14,7 +15,7 @@ useEffect(() => {
     modifyCar("/cars",updateCar.id,data)
   }
   else{
-    addCar("/cars/",data)
+    addCar("/cars",data)
   }
 
   reset({
@@ -29,24 +30,24 @@ useEffect(() => {
   return (
     <form onSubmit={handleSubmit(submit)}>
         <h2>Card Form</h2>
-        <label htmlFor="brand">Brand</label>
-        <input type="text" id='brand' {...register('brand')} required/>
         
-        <label htmlFor="model">model</label>
-        <input type="text" id='model'{...register('model')} required/>
+        <input placeholder='Brand' type="text" id='brand' {...register('brand')} required/>
         
-        <label htmlFor="color">color</label>
-        <input type="text" id='color'{...register('color')} required/>
+        
+        <input placeholder='Model' type="text" id='model'{...register('model')} required/>
+        
+        
+        <input placeholder='Color' type="text" id='color'{...register('color')} required/>
 
-        <label htmlFor="year">year</label>
-        <input type="text" id='year'{...register('year')} required/>
+      
+        <input placeholder='Year' type="text" id='year'{...register('year')} required/>
         
-        <label htmlFor="price">price</label>
-        <input type="text" id='price' {...register('price')} required/>
         
-        <label htmlFor="image">image</label>
-        <input type="text" id='image'{...register('image')} required/>
-        <button>submit</button>
+        <input placeholder='Price' type="text" id='price' {...register('price')} required/>
+        
+        
+        <input placeholder='Image' type="text" id='image'{...register('image')} required/>
+        <button className='submit'>submit</button>
     </form>
   )
 }
